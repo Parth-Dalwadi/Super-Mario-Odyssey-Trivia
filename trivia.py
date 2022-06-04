ea3cda0c7efa496683557d6cf2c3dfb4b3aac64e
Parth Dalwadi <dalwadip77@gmail.com>
2022-06-04 15:04:54 -0400

    Added music stop feature, opened json file, and randomized content in json file

--- trivia.py
+++ trivia.py
@@ -2,9 +2,32 @@ import json
 from tkinter import *
 from tkinter import ttk
 import winsound
+import random
 
 
 winsound.PlaySound('Music/steamGardens.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
+
+# Music
+
+
+def stop_music():
+    winsound.PlaySound(None, winsound.SND_ASYNC)
+
+
+def change_music():
+    winsound.PlaySound(None, winsound.SND_ASYNC)
+
+
+# Trivia Game
+with open('triviaQ&A.json') as file:
+    section = json.load(file)
+
+questions = section['question']
+choices = section['choices']
+answers = section['answer']
+zipQCA = zip(questions, choices, answers)
+listQCA = list(zipQCA)
+random.shuffle(listQCA)
 
 root = Tk()
 width = root.winfo_screenwidth()
@@ -16,6 +39,7 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!", background="red").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="Stop Music", command=stop_music).grid(column=2, row=0)
 
 
 class Trivia:
