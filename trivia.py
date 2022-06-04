fcce18971d0c36d28c599478e1fb67575313390d
Parth Dalwadi <dalwadip77@gmail.com>
2022-06-03 23:44:12 -0400

    Added tkinter, set up basic grid with buttons, set up width & height, added music for windows users

--- trivia.py
+++ trivia.py
@@ -1,2 +1,32 @@
 import json
+from tkinter import *
+from tkinter import ttk
+import winsound
+
+
+winsound.PlaySound('Music/steamGardens.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
+
+root = Tk()
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.geometry("%dx%d" % (width, height))
+root.title("Super Mario Odyssey Trivia")
+root.configure(background="black")
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!", background="red").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+
+class Trivia:
+    def __init__(self):
+        self
+
+
+root.mainloop()
+winsound.PlaySound(None, winsound.SND_PURGE)
+
+
+
+
 
