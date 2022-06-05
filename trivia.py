0485557fd8f7c0e7bfe2831741edfaabe781250a
Parth Dalwadi <dalwadip77@gmail.com>
2022-06-05 00:32:14 -0400

    Added new json file for music, randomized music from json file, created change_music function

--- trivia.py
+++ trivia.py
@@ -6,14 +6,19 @@ import random
 
 
 # Music
-winsound.PlaySound('Music/steamGardens.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
+with open('musicNames.json') as music_file:
+    music_obj = json.load(music_file)
+
+
+music_names = music_obj['names']
+list_music = list(music_names)
+random.shuffle(list_music)
+
+
+winsound.PlaySound('Music/' + list_music[0] + '.wav', winsound.SND_FILENAME | winsound.SND_ASYNC)
 
 
 def stop_music():
-    winsound.PlaySound(None, winsound.SND_ASYNC)
-
-
-def change_music():
     winsound.PlaySound(None, winsound.SND_ASYNC)
 
 
@@ -43,13 +48,26 @@ title.pack(side="top")
 class Trivia:
     def __init__(self):
         self.buttons()
+        self.music_count = 0
+        self.music_name = ''
 
     def buttons(self):
-        quit_button = Button(root, text="Quit", command=root.destroy, width=10, bg="red", fg="white", font=("Helvetica", 16, "bold"))
+        quit_button = Button(root, text="Quit", command=root.destroy, width=12, bg="red", fg="white", font=("Helvetica", 16, "bold"))
         quit_button.place(x=100, y=200)
 
-        stop_music_button = Button(root, text="Stop Music", command=stop_music, width=10, bg="green", fg="white", font=("Helvetica", 16, "bold"))
+        stop_music_button = Button(root, text="Stop Music", command=stop_music, width=12, bg="green", fg="white", font=("Helvetica", 16, "bold"))
         stop_music_button.place(x=300, y=200)
+
+        change_music_button = Button(root, text="Change Music", command=self.change_music, width=12, bg="green", fg="white", font=("Helvetica", 16, "bold"))
+        change_music_button.place(x=500, y=200)
+
+    def change_music(self):
+        self.music_count += 1
+        if self.music_count == len(list_music):
+            self.music_count = 0
+        self.music_name = list_music[self.music_count]
+        music_string = 'Music/' + self.music_name + '.wav'
+        winsound.PlaySound(music_string, winsound.SND_FILENAME | winsound.SND_ASYNC)
 
 
 trivia = Trivia()
